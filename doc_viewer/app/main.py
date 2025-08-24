58d1dc086e3ee5af4e8098e94676f643f7c494ea
nevesrodrigo2 <nevesrodrigo2@gmail.com>
2025-08-24 13:40:12 +0100

    restructure main window and create domain controllers

--- doc_viewer/app/main.py
+++ doc_viewer/app/main.py
@@ -8,6 +8,9 @@ from doc_viewer.settings.logger import get_logger
 from doc_viewer.settings.config import APP_NAME
 from doc_viewer.ui.main_window import MainWindow
 
+from doc_viewer.domain.models.document import DocumentController
+from doc_viewer.domain.models.folder import FolderController
+
 # start logger
 logger = get_logger(__name__)
 
@@ -16,8 +19,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--debug', action='store_true', help='Run in debug mode')
 args = parser.parse_args()
 
+# document controller
+doc_controller = DocumentController()
+
+# folder controller
+folder_controller = FolderController()
+
+# setup ui
 app = QApplication([])
 window = MainWindow(APP_NAME)
-window.show()
 
 app.exec()
--- doc_viewer/ui/main_window.py
+++ doc_viewer/ui/main_window.py
@@ -1,3 +1,4 @@
+# PySide6 Imports
 from PySide6.QtWidgets import (
     QWidget, 
     QMainWindow, 
@@ -5,25 +6,40 @@ from PySide6.QtWidgets import (
     QHBoxLayout
 )
 
+# Project Imports
 from doc_viewer.ui.components.menu_bar import MenuBar
+from doc_viewer.ui.components.tab_group.tab_group import TabGroup
+from doc_viewer.ui.components.document_viewer.document_stack import DocumentViewerStack
 
 class MainWindow(QMainWindow):
+    """Main application window."""
     def __init__(self, app_name: str):
         super().__init__()
         self.setWindowTitle(app_name)
 
+        self.setup_main_window()
+        self.show()
+
+    def setup_main_window(self):
+        """Setup the main window layout and components."""
+
         # Create a central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-
-        # Create layout and assign it to the central widget
         self.layout = QVBoxLayout(central_widget)
 
         # set menu bar
         self.menu_bar = MenuBar()
-
         self.layout.setMenuBar(self.menu_bar)
 
+        # set main layout
         self.main_layout = QHBoxLayout()
-
         self.layout.addLayout(self.main_layout)
+
+        # tab group
+        self.tab_group = TabGroup()
+        self.main_layout.addWidget(self.tab_group, 1)
+        # document viewer stack
+        self.doc_viewer_stack = DocumentViewerStack()
+        self.main_layout.addWidget(self.doc_viewer_stack, 4)
+
